3fc73e0da17d9b5cd5dbf32de211ceb45740f1e4
Szidelo <claudiuszidelo@yahoo.ro>
2023-11-08 18:36:01 +0200

    test

--- src/Pages/NotFound.js
+++ src/Pages/NotFound.js
@@ -2,7 +2,7 @@ const NotFound = () => {
     return (
         <div className="not-found-page d-flex flex-column justify-content-center align-items-center">
             <h1 className="heading-xl">404: Page Not Found</h1>
-            <a className="link-1 body-2 mt-4" href='/'>Return to homepage</a>
+            <a className="btn-dine mt-4" href='/'>Return to homepage</a>
         </div>
     )
 }
